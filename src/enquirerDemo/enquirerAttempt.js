dd6a9f593d7415893380cca1128ab256ee8b0ae0
nbogie <neillbogie@googlemail.com>
2024-01-08 15:55:58 +0000

    misc

--- src/enquirerDemo/enquirerAttempt.js
+++ src/enquirerDemo/enquirerAttempt.js
@@ -4,6 +4,4 @@ prompt({
     type: "input",
     message: "What is your favourite colour?",
     name: "faveColour",
-}).then((response) => {
-    console.log("You said: ", response.faveColour);
 });
