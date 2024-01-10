f31b37e035257917d000f719994f1eb6a55e7a5e
nbogie <neillbogie@googlemail.com>
2024-01-10 17:52:00 +0000

    improve clarity of axios example

--- src/axiosDemo/axiosDemo1WithThen.js
+++ src/axiosDemo/axiosDemo1WithThen.js
@@ -2,18 +2,18 @@ const axios = require("axios").default;
 
 function mainTask() {
     const url = "https://jsonplaceholder.typicode.com/users/1"; //or https://swapi.dev/api/people/1
-
     const resultingPromise = axios.get(url);
-    console.log("fetch started");
+    console.log("Asked axios to get " + url);
     resultingPromise.then(handleCompletedPromise);
 }
 
 function handleCompletedPromise(result) {
-    console.log("promise completed");
-    console.log("The result was an object with .status and .data as follows: ");
-    console.log(result.status);
-    console.log(result.data);
-    console.log("fetch completed");
+    console.log("Promise completed");
+    console.log("The result was an object including ");
+    console.log(".status: ", result.status);
+    console.log(".data: ", result.data);
 }
 
 mainTask();
+
+console.log("after mainTask()");
